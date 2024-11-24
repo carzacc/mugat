f37cff30b7812ca141d56d27c1fcf2ec3706e62d
Carmine Zaccagnino <carmine@carminezacc.com>
2024-11-24 09:21:53 +0100

    started work on true multipage modeling
    Signed-off-by: Carmine Zaccagnino <carmine@carminezacc.com>

--- nougat/__init__.py
+++ nougat/__init__.py
@@ -4,12 +4,13 @@ Copyright (c) 2022-present NAVER Corp.
 MIT License
 Copyright (c) Meta Platforms, Inc. and affiliates.
 """
-from .model_adapted import NougatConfig, NougatModel
+from .model_adapted_extreme import NougatConfig, NougatModel
 from .utils.dataset import NougatDataset
+from .utils.tpage_dataset import ThreePageDataset
 from ._version import __version__
 
 __all__ = [
     "NougatConfig",
     "NougatModel",
-    "NougatDataset",
+    "ThreePageDataset",
 ]
